66aca6e1e7065540a039c569b698bc0f64065e41
peggy.shi <peggy.shi@yxunistar.com>
2020-04-10 17:21:22 +0800

    PF-17197 publish 1.3.1

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ export { default as Theme } from "components/Theme";
 export { default as Button } from "components/Button";
 export { default as Divider } from "components/Divider";
 export { default as Highlight } from "components/Highlight";
+export { default as Icon } from "components/Icon";
+export { default as List } from "components/List";
