d99a15bc8816182f0891bac2de7d7018f1028956
peggy.shi <peggy.shi@yxunistar.com>
2020-04-14 18:45:55 +0800

    PF-17197 add green type of highlight

--- src/components/Highlight/index.js
+++ src/components/Highlight/index.js
@@ -1,7 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
+import "./style.sass";
+
+const TYPE = {
+    RED: "red",
+    GREEN: "green",
+};
 
 const Highlight = props => {
-    return <span style={{ color: "#ff5243" }}>{props.children}</span>;
+    return <span className={cx("unistar-highlight", props.type)}>{props.children}</span>;
+};
+
+Highlight.propTypes = {
+    type: PropTypes.string,
+};
+
+Highlight.defaultProps = {
+    type: TYPE.RED,
 };
 
 export default Highlight;
--- src/components/Highlight/stories/Examples.js
+++ src/components/Highlight/stories/Examples.js
@@ -5,10 +5,15 @@ import Highlight from "Highlight";
 const Examples = () => {
     return (
         <>
-            <Block title="Default">
-                <Row>
-                    這段文字<Highlight>高調顯示</Highlight>的模樣。
-                </Row>
+            <Block title="Type">
+                <>
+                    <Row>
+                        這段文字<Highlight>高調顯示(red: Default)</Highlight>的模樣。
+                    </Row>
+                    <Row>
+                        這段文字<Highlight type="green">高調顯示(green)</Highlight>的模樣。
+                    </Row>
+                </>
             </Block>
         </>
     );
