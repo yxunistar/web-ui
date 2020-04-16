be23c930c6ece435f129bbcb4a0a9bc641d96e67
peggy.shi <peggy.shi@yxunistar.com>
2020-04-16 18:53:01 +0800

    PF-17197 update radio wording

--- src/components/Radio/stories/index.stories.js
+++ src/components/Radio/stories/index.stories.js
@@ -14,11 +14,11 @@ const document = () => (
 
 const examples = () => (
     <>
-        <Block title="Radio" description={`純粹顯示用`} demo={RadioDemo} />
-        <Block title="Radio Group" description={`透過 RadioGroup 指定 Radio 的 checked 狀態`} demo={RadioGroupDemo} />
-        <Block title="Size" description={`尺寸有兩種型態：large (default), small`} demo={SizeDemo} />
-        <Block title="Disabled" demo={DisabledDemo} />
-        <Block title="Theme" description={`指定日夜版：early, night，不受環境影響`} demo={ThemeDemo} />
+        <Block title="Radio" description={`純粹顯示用。`} demo={RadioDemo} />
+        <Block title="Radio Group" description={`透過 RadioGroup 指定 Radio 的 checked 狀態。`} demo={RadioGroupDemo} />
+        <Block title="Size" description={`尺寸有兩種型態：large (default), small，也可直接指定在 Radio 上。`} demo={SizeDemo} />
+        <Block title="Disabled" description={`也可直接指定在 Radio 上。`} demo={DisabledDemo} />
+        <Block title="Theme" description={`指定日夜版：early, night，不受環境影響，也可直接指定在 Radio 上。`} demo={ThemeDemo} />
     </>
 );
 
