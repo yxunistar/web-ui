fa1c0e8f8359576b40f5dca28062f769120fe441
peggy.shi <peggy.shi@yxunistar.com>
2020-04-17 12:19:44 +0800

    add datepicker and radio index and update global font style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line filenames/match-exported
-export { default as Theme } from "components/Theme";
 export { default as Button } from "components/Button";
+export { default as DatePicker } from "components/DatePicker";
 export { default as Divider } from "components/Divider";
+export { default as Form } from "components/Form";
 export { default as Highlight } from "components/Highlight";
 export { default as Icon } from "components/Icon";
-export { default as List } from "components/List";
-export { default as Form } from "components/Form";
 export { default as Layout } from "components/Layout";
+export { default as List } from "components/List";
+export { default as Radio } from "components/Radio";
+export { default as Theme } from "components/Theme";
