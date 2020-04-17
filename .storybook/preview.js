047b2797ceb0ff6b265ef86b21d412ca6ca09cf5
peggy.shi <peggy.shi@yxunistar.com>
2020-04-17 16:08:05 +0800

    PF-17197 sort category

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,5 +7,7 @@ addDecorator(storyFn => <ThemeProvider>{storyFn()}</ThemeProvider>);
 addParameters({
     options: {
         showRoots: true,
+        storySort: (a, b) =>
+            a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
     },
 });
