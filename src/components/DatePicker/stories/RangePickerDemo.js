46e8e0660bdd7701808e05f1c2c3ac2d40517740
wadezhu <wade.chu@yxunistar.com>
2020-05-19 17:30:38 +0800

    Add datepicker wording

--- src/components/DatePicker/stories/RangePickerDemo.js
+++ src/components/DatePicker/stories/RangePickerDemo.js
@@ -2,7 +2,27 @@ import React from "react";
 import DatePicker from "DatePicker";
 
 const RangePickerDemo = () => {
-    return <DatePicker.RangePicker />;
+    const today = new Date();
+    const state = {
+        start: {
+            minDate: "",
+            maxDate: today,
+            date: today,
+        },
+        end: {
+            minDate: "",
+            maxDate: today,
+            date: today,
+        },
+    };
+    return (
+        <DatePicker.RangePicker
+            start={state.start}
+            end={state.end}
+            onStartDateChange={startDate => this.handleStartDateChange(startDate)}
+            onEndDateChange={endDate => this.handleEndDateChange(endDate)}
+        />
+    );
 };
 
 export default RangePickerDemo;
--- src/components/DatePicker/stories/RangePickerFilterDemo.js
+++ src/components/DatePicker/stories/RangePickerFilterDemo.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import DatePicker from "DatePicker";
+import Dater from "utils/Dater";
 
 const RangePickerFilterDemo = () => {
-    return <DatePicker.RangePickerFilter />;
+    const today = new Date();
+    const [start, setStart] = useState(Dater.format(today, { format: "YYYY-MM-DD" }));
+    const [end, setEnd] = useState(Dater.format(today, { format: "YYYY-MM-DD" }));
+
+    const onStartDateChange = startDate => {
+        setStart(startDate);
+    };
+
+    const onEndDateChange = endDate => {
+        setEnd(endDate);
+    };
+
+    const onFilterChange = (startDate, endDate) => {
+        setStart(startDate);
+        setEnd(endDate);
+    };
+
+    return (
+        <>
+            <p>
+                startDate: {start} , endDate: {end}
+            </p>
+            <DatePicker.RangePickerFilter
+                onStartDateChange={onStartDateChange}
+                onEndDateChange={onEndDateChange}
+                onFilterChange={onFilterChange}
+            />
+        </>
+    );
 };
 
 export default RangePickerFilterDemo;
