ba204c77130bd396ed26607f92f04a79a6448992
wadezhu <wade.chu@yxunistar.com>
2020-05-21 16:21:37 +0800

    fixed import url

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export { default as DatePicker } from "components/DatePicker";
 export { default as Divider } from "components/Divider";
 export { default as Form } from "components/Form";
 export { default as Highlight } from "components/Highlight";
-export { default as Icon } from "./components/Select/node_modules/components/Icon";
+export { default as Icon } from "components/Icon";
 export { default as Layout } from "components/Layout";
 export { default as List } from "components/List";
 export { default as Radio } from "components/Radio";
