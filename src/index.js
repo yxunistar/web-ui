4c9f7d86a4bb01eb9d68cdf9a2fcfc5785c0843e
Waltor.Huang <Waltor.Huang@yxunistar.com>
2020-07-14 14:52:59 +0800

    insert missing import statement

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 // eslint-disable-next-line filenames/match-exported
+export { default as Badge } from "./components/Badge";
 export { default as Button } from "./components/Button";
 export { default as DatePicker } from "./components/DatePicker";
+export { default as Dice } from "./components/Dice";
 export { default as Divider } from "./components/Divider";
 export { default as Form } from "./components/Form";
 export { default as Highlight } from "./components/Highlight";
 export { default as Icon } from "./components/Icon";
+export { default as Input } from "./components/Input";
 export { default as Layout } from "./components/Layout";
 export { default as List } from "./components/List";
 export { default as Radio } from "./components/Radio";
-export { default as Theme } from "./components/Theme";
-export { default as Input } from "./components/Input";
-export { default as Dice } from "./components/Dice";
 export { default as Select } from "./components/Select";
+export { default as SplitLayout } from "./components/SplitLayout";
+export { default as Tag } from "./components/Tag";
+export { default as Theme } from "./components/Theme";
